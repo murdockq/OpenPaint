d967c5cf3ed8c53de8fd852d022d8041832bf0b0
S J <>
2026-06-23 23:48:06 -0400

    Update copyright information to reflect OpenPaint ownership

--- OpenPaint/Globals.cpp
+++ OpenPaint/Globals.cpp
@@ -2,7 +2,7 @@
  * OpenPaint
  * www.sourceforge.net/projects/OpenPaint
  *
- * Copyright (C) 2007, Steven Jedlicka
+ * Copyright (C) 2007, OpenPaint
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
